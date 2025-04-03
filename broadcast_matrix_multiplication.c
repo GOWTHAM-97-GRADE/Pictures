161fbdeabeb38effc837d0f63cbcd80903a648ee
Gowtham P <121608685+GOWTHAM-97-GRADE@users.noreply.github.com>
2025-04-03 07:26:50 +0530

    ""

--- broadcast_matrix_multiplication.c
+++ broadcast_matrix_multiplication.c
@@ -1,97 +1,77 @@
-#include <stdio.h>
+
 #include <mpi.h>
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 3  // 3x3 Matrix
+#define N 4
 
-void printMatrix(int mat[SIZE][SIZE]) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            printf("%d\t", mat[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-// Function to get valid integer input
-void getMatrixInput(int matrix[SIZE][SIZE], char name) {
-    printf("Enter values for Matrix %c (%dx%d):\n", name, SIZE, SIZE);
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            while (scanf("%d", &matrix[i][j]) != 1) {
-                printf("Invalid input. Please enter an integer: ");
-                while (getchar() != '\n'); // Clear input buffer
-            }
+void multiply_row(int row[], int B[N][N], int result[], int n) {
+    for (int j = 0; j < n; j++) {
+        result[j] = 0;
+        for (int k = 0; k < n; k++) {
+            result[j] += row[k] * B[k][j];
         }
     }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
     int rank, size;
-    int A[SIZE][SIZE], B[SIZE][SIZE];
-    int C[SIZE][SIZE] = {0};  // Result Matrix
+    int A[N][N], B[N][N], C[N][N]; // Matrices
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Calculate number of rows each process should handle
-    int rowsPerProcess = SIZE / size;
-    int leftoverRows = SIZE % size;
-
-    // Compute startRow and endRow for each process
-    int startRow = rank * rowsPerProcess + (rank < leftoverRows ? rank : leftoverRows);
-    int endRow = startRow + rowsPerProcess + (rank < leftoverRows ? 1 : 0);
-    int numRows = endRow - startRow; // Number of rows assigned to this process
-
-    // Master process takes user input for matrices A and B
-    if (rank == 0) {
-        getMatrixInput(A, 'A');
-        getMatrixInput(B, 'B');
+    if (N % size != 0) {
+        if (rank == 0) {
+            printf("Error: Number of processes must evenly divide N (%d).\n", N);
+        }
+        MPI_Finalize();
+        exit(1);
     }
 
-    // Broadcast matrices A and B to all processes
-    MPI_Bcast(A, SIZE * SIZE, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(B, SIZE * SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+    int rows_per_proc = N / size;
+    int sub_A[rows_per_proc][N], sub_C[rows_per_proc][N];
 
-    // Each process computes its assigned rows
-    int myResult[numRows][SIZE];  // Local result matrix
-    memset(myResult, 0, sizeof(myResult));
+    if (rank == 0) {
+        printf("Enter elements for matrix A (%dx%d):\n", N, N);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (scanf("%d", &A[i][j]) != 1) {
+                    printf("Invalid input for A[%d][%d]. Exiting.\n", i, j);
+                    MPI_Abort(MPI_COMM_WORLD, 1);
+                }
+            }
+        }
 
-    for (int i = startRow; i < endRow; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            for (int k = 0; k < SIZE; k++) {
-                myResult[i - startRow][j] += A[i][k] * B[k][j];
+        printf("Enter elements for matrix B (%dx%d):\n", N, N);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (scanf("%d", &B[i][j]) != 1) {
+                    printf("Invalid input for B[%d][%d]. Exiting.\n", i, j);
+                    MPI_Abort(MPI_COMM_WORLD, 1);
+                }
             }
         }
     }
 
-    // Prepare arrays for MPI_Gatherv
-    int sendcounts[size], displs[size];
-    if (rank == 0) {
-        for (int i = 0, pos = 0; i < size; i++) {
-            int rStart = i * rowsPerProcess + (i < leftoverRows ? i : leftoverRows);
-            int rEnd = rStart + rowsPerProcess + (i < leftoverRows ? 1 : 0);
-            sendcounts[i] = (rEnd - rStart) * SIZE;
-            displs[i] = pos;
-            pos += sendcounts[i];
-        }
+    MPI_Bcast(B, N * N, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(A, rows_per_proc * N, MPI_INT, sub_A, rows_per_proc * N, MPI_INT, 0, MPI_COMM_WORLD);
+
+    for (int i = 0; i < rows_per_proc; i++) {
+        multiply_row(sub_A[i], B, sub_C[i], N);
     }
 
-    // Gather the results correctly from all processes
-    MPI_Gatherv(myResult, numRows * SIZE, MPI_INT, C, sendcounts, displs, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gather(sub_C, rows_per_proc * N, MPI_INT, C, rows_per_proc * N, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Master process prints the result
     if (rank == 0) {
-        printf("\nMatrix A:\n");
-        printMatrix(A);
-
-        printf("\nMatrix B:\n");
-        printMatrix(B);
-
-        printf("\nMatrix C (Result of A * B):\n");
-        printMatrix(C);
+        printf("\nResult Matrix (Broadcast & Gather):\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                printf("%d ", C[i][j]);
+            }
+            printf("\n");
+        }
     }
 
     MPI_Finalize();
--- pt_to_pt_matrix_multiplication.c
+++ pt_to_pt_matrix_multiplication.c
@@ -1,90 +1,85 @@
-#include <stdio.h>
 #include <mpi.h>
-#define SIZE 3
+#include <stdio.h>
+#include <stdlib.h>
 
-void printMatrix(int mat[SIZE][SIZE]) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            printf("%d\t", mat[i][j]);
+#define N 4
+
+void multiply_row(int row[], int B[N][N], int result[], int n) {
+    for (int j = 0; j < n; j++) {
+        result[j] = 0;
+        for (int k = 0; k < n; k++) {
+            result[j] += row[k] * B[k][j];
         }
-        printf("\n");
     }
 }
 
-int inputMatrix(int mat[SIZE][SIZE], const char* name) {
-    printf("Enter elements for %s (%dx%d matrix):\n", name, SIZE, SIZE);
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            printf("%s[%d][%d] = ", name, i, j);
-            if (scanf("%d", &mat[i][j]) != 1) {
-                printf("Error: Invalid input\n");
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
-
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
     int rank, size;
-    int A[SIZE][SIZE];
-    int B[SIZE][SIZE];
-    int C[SIZE][SIZE] = {0};
+    int A[N][N], B[N][N], C[N][N];
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (size < 2) {
-        if (rank == 0) {
-            printf("Error: Program needs at least 2 processes\n");
-        }
-        MPI_Finalize();
-        return 1;
-    }
+    
+
+    int rows_per_proc = N / size;
+    int sub_A[rows_per_proc][N], sub_C[rows_per_proc][N];
 
     if (rank == 0) {
-        // Get user input for matrices
-        if (inputMatrix(A, "Matrix A") || inputMatrix(B, "Matrix B")) {
-            MPI_Finalize();
-            return 1;
+        printf("Enter elements for matrix A (%dx%d):\n", N, N);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (scanf("%d", &A[i][j]) != 1) {
+                    printf("Invalid input for A[%d][%d]. Exiting.\n", i, j);
+                    MPI_Abort(MPI_COMM_WORLD, 1);
+                }
+            }
         }
-
-        // Send data to worker processes
-        for (int i = 1; i < size && i <= SIZE; i++) {
-            MPI_Send(A[i-1], SIZE, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(B, SIZE * SIZE, MPI_INT, i, 0, MPI_COMM_WORLD);
-        }
-
-        // Receive results
-        for (int i = 1; i < size && i <= SIZE; i++) {
-            MPI_Recv(C[i-1], SIZE, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-
-        // Print results
-        printf("\nMatrix A:\n");
-        printMatrix(A);
-        printf("\nMatrix B:\n");
-        printMatrix(B);
-        printf("\nMatrix C (Result of A * B):\n");
-        printMatrix(C);
-    }
-    else if (rank <= SIZE) {  // Worker processes (only up to rank 3 needed)
-        int myRow[SIZE];
-        int myResult[SIZE] = {0};
-        int B_local[SIZE][SIZE];
-
-        MPI_Recv(myRow, SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(B_local, SIZE * SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        // Perform multiplication for this row
-        for (int j = 0; j < SIZE; j++) {
-            for (int k = 0; k < SIZE; k++) {
-                myResult[j] += myRow[k] * B_local[k][j];
+        
+        printf("Enter elements for matrix B (%dx%d):\n", N, N);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (scanf("%d", &B[i][j]) != 1) {
+                    printf("Invalid input for B[%d][%d]. Exiting.\n", i, j);
+                    MPI_Abort(MPI_COMM_WORLD, 1);
+                }
             }
         }
 
-        MPI_Send(myResult, SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        for (int i = 1; i < size; i++) {
+            MPI_Send(&A[i * rows_per_proc], rows_per_proc * N, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+    } else {
+        MPI_Recv(sub_A, rows_per_proc * N, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+    MPI_Bcast(B, N * N, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    for (int i = 0; i < rows_per_proc; i++) {
+        multiply_row(sub_A[i], B, sub_C[i], N);
+    }
+
+    if (rank == 0) {
+        for (int i = 0; i < rows_per_proc; i++) {
+            for (int j = 0; j < N; j++) {
+                C[i][j] = sub_C[i][j];
+            }
+        }
+
+        for (int i = 1; i < size; i++) {
+            MPI_Recv(&C[i * rows_per_proc], rows_per_proc * N, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        printf("\nResult Matrix C (point-to-point):\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                printf("%d ", C[i][j]);
+            }
+            printf("\n");
+        }
+    } else {
+        MPI_Send(sub_C, rows_per_proc * N, MPI_INT, 0, 1, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
